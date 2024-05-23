46668ac44f13bd30e7d8f32adcf734c72297b165
Manish Shivabhakti <manish.shivabhakti.99@gmail.com>
2024-05-23 10:11:54 +0545

    chore: Add BottomSheetModalProvider for better modal handling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { NativeWindStyleSheet } from 'nativewind';
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
 NativeWindStyleSheet.setOutput({
 	default: 'native',
@@ -8,44 +9,46 @@ NativeWindStyleSheet.setOutput({
 
 export default function HomeLayout() {
 	return (
-		<Tabs>
-			<Tabs.Screen
-				name='index'
-				options={{
-					title: 'Home',
-					tabBarIcon: ({ color }) => (
-						<Ionicons name='home-outline' size={16} color={color} />
-					),
-					headerShown: false,
-				}}
-			/>
-			<Tabs.Screen
-				name='reminders/index'
-				options={{
-					title: 'Reminders',
-					tabBarIcon: ({ color }) => (
-						<Ionicons name='alarm-outline' size={16} color={color} />
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name='report/index'
-				options={{
-					title: 'Report',
-					tabBarIcon: ({ color }) => (
-						<Ionicons name='analytics-outline' size={16} color={color} />
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name='settings/index'
-				options={{
-					title: 'Settings',
-					tabBarIcon: ({ color }) => (
-						<Ionicons name='settings-outline' size={16} color={color} />
-					),
-				}}
-			/>
-		</Tabs>
+		<BottomSheetModalProvider>
+			<Tabs>
+				<Tabs.Screen
+					name='index'
+					options={{
+						title: 'Home',
+						tabBarIcon: ({ color }) => (
+							<Ionicons name='home-outline' size={16} color={color} />
+						),
+						headerShown: false,
+					}}
+				/>
+				<Tabs.Screen
+					name='reminders/index'
+					options={{
+						title: 'Reminders',
+						tabBarIcon: ({ color }) => (
+							<Ionicons name='alarm-outline' size={16} color={color} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name='report/index'
+					options={{
+						title: 'Report',
+						tabBarIcon: ({ color }) => (
+							<Ionicons name='analytics-outline' size={16} color={color} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name='settings/index'
+					options={{
+						title: 'Settings',
+						tabBarIcon: ({ color }) => (
+							<Ionicons name='settings-outline' size={16} color={color} />
+						),
+					}}
+				/>
+			</Tabs>
+		</BottomSheetModalProvider>
 	);
 }
